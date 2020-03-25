b41315a507e47318dd3ad1f6d469228bcf5dcf15
secondriver <413733843@qq.com>
2020-03-25 23:59:13 +0800

    Thread

--- thread/TextRunnable.java
+++ thread/TextRunnable.java
@@ -7,21 +7,21 @@ import javax.imageio.stream.ImageInputStream;
  * @date: 2020/3/24 18:15
  */
 
-// 实现 Runnable 接口，重写 run() 方法，执行线程需要丢入 Runnable 接口实现类，调用 start 方法
-public class TextRunnable implements Runnable {
+// 实现 Runnable 接口，重写 run() 方法，创建 Runnable 接口实现类对象，创建线程对象传入接口实现类对象，调用 start 方法
+public class TextRunnable implements Runnable{
     @Override
     public void run() {
-        for (int i = 0; i < 200; i++) {
-            System.out.println("我在学 Java————" + i);
+        for (int i =0; i < 200; i++) {
+            System.out.println("我在睡觉————"+i);
         }
     }
 
     public static void main(String[] args) {
         TextRunnable textRunnable = new TextRunnable();
-        Thread thread = new Thread(textRunnable);
-        thread.start();
+        new Thread(textRunnable).start();
+
         for (int i = 0; i < 1000; i++) {
-            System.out.println("我在吃饭——————" + i);
+            System.out.println("我在学习多线程"+i);
         }
     }
 }
---
+++ thread/Thread_ticket.java
@@ -0,0 +1,41 @@
+package thread;
+
+import java.util.function.DoubleToIntFunction;
+
+/**
+ * @author: Dennis
+ * @date: 2020/3/25 15:26
+ */
+
+/*
+多线程操作同一个对象
+
+问题：多线程操作同一个资源时，线程不安全，数据紊乱
+ */
+public class Thread_ticket implements Runnable {
+    private int ticketNum = 1;
+
+    @Override
+    public void run() {
+        while (true){
+            if (ticketNum >= 10){
+                break;
+            }
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "--拿到第" + ticketNum++ + "票");
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread_ticket thread_ticket = new Thread_ticket();
+
+        new Thread(thread_ticket,"小明").start();
+        new Thread(thread_ticket,"老师").start();
+        new Thread(thread_ticket,"黄牛").start();
+
+    }
+}
