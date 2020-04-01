12dec28cc914339413fe1386686c98c3137097d9
secondriver <413733843@qq.com>
2020-04-01 23:20:56 +0800

    text

--- text/fanXing.java
+++ text/fanXing.java
@@ -1,32 +1,23 @@
 package text;
 
+import java.lang.reflect.Array;
+
 /**
  * @author: Dennis
  * @date: 2020/3/22 23:12
  */
 // T 被称作类型参数，可以只带任何类型(不包括基本类型)
 class Myclass<T>{
-    private T x;
 
-    public T getX() {
-        return x;
-    }
-    public  void setX(T x){
-        this.x = x;
-    }
 }
 
-class myclass{
-    public <T> T text(T t){
-
-    }
+public class fanXing<T> {
+public T[] creat(Class<T> t){
+    return (T[])Array.newInstance(t,10);
 }
-public class fanXing {
+
     public static void main(String[] args) {
-        Myclass<String> D = new Myclass<>();
-        D.setX("泛型使用");
-        String x = D.getX();
-        System.out.println(x);
+        fanXing<String> f = new fanXing<>();
+        String[] string = f.creat(String.class);
     }
-
 }
