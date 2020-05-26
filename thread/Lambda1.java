56e31e0b72f02aeff446f7b8068fde3c34172215
secondriver <413733843@qq.com>
2020-05-26 23:57:51 +0800

    thread_test

---
+++ thread/Lambda1.java
@@ -0,0 +1,59 @@
+package thread;
+
+/**
+ * @author: Dennis
+ * @date: 2020/3/25 18:18
+ */
+
+// 1、定义一个函数式接口
+interface ILike {
+    void lambda();
+}
+
+// 2、实现类
+class Like implements ILike {
+    @Override
+    public void lambda() {
+        System.out.println("i like lambda1");
+    }
+}
+
+
+public class Lambda1 {
+
+    public static void main(String[] args) {
+        // 接口 new 了一个实现类
+        ILike like = new Like();
+        like.lambda();
+
+
+        // 3.局部内部类
+        class Like3 implements ILike {
+            @Override
+            public void lambda() {
+                System.out.println("i like lambda2");
+            }
+        }
+        like = new Like3();
+        like.lambda();
+
+
+        // 4、匿名内部类，没有类的名称，必须借助接口、父类
+        like = new ILike() {
+            @Override
+            public void lambda() {
+                System.out.println("i like lambda3");
+            }
+        };
+        like.lambda();
+
+
+        // 5、 用 Lambda 简化
+        like = () -> {
+            System.out.println("i like lambda4");
+        };
+        like.lambda();
+    }
+}
+
+
---
+++ thread/test.java
@@ -0,0 +1,13 @@
+package thread;
+
+/**
+ * @author: Dennis
+ * @date: 2020/5/25 14:30
+ */
+
+public class test {
+    public static void main(String[] args) {
+        Thread thread = new Thread();
+
+    }
+}
